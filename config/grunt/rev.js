2f5bcff45b7dae23ec6b794029d3fb3bbc67fde1
zhi.zhong <zhi.zhong@qunar.com>
2015-05-05 17:11:56 +0800

    Remove zh-cn files

--- config/grunt/rev.js
+++ config/grunt/rev.js
@@ -4,8 +4,7 @@ module.exports = {
   dist: {
     files: {
       src: [
-        '<%=yo.dist%>/static/**/*.{js,css,png,jpg,jpeg,gif,ttf}',
-        '!<%=yo.dist%>/static/js/app/nls/zh-cn/colors.js'
+        '<%=yo.dist%>/static/**/*.{js,css,png,jpg,jpeg,gif,ttf}'
       ]
     }
   }
