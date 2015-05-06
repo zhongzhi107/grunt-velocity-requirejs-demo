f3773bf3fb285c730fa54964a279293528e02590
zhi.zhong <zhi.zhong@163.com>
2015-05-06 10:43:41 +0800

    Serve task

--- Gruntfile.js
+++ Gruntfile.js
@@ -197,7 +197,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('server', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run([target ? ('serve:' + target) : 'serve']);
+    grunt.task.run(['serve' + (target ? ':' + target : '')]);
   });
 
   // 注册代码编译任务
