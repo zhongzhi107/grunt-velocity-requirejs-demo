c9362448b7f45fc25827e62b49a0cba9b1e9542b
zhongzhi107 <zhong.zhi@163.com>
2015-05-08 12:26:41 +0800

    Update test.js

--- app/data/api/home/test.js
+++ app/data/api/home/test.js
@@ -1,9 +1,26 @@
+'use strict';
 
+var url = require('url');
+var querystring = require('querystring');
 
-module.exports = function (_GET) {
-	return {
-		req: {
-			source: 'dev'
-		}
-	};
-};
+module.exports = function(req, res) {
+  var query = querystring.parse(url.parse(req.url).query);
+  var limit = query.limit || 5;
+  //console.log(query);
+  var data = {
+    list: []
+  };
+  for (var i = 0; i < limit; i++) {
+    data.list.push({
+      id: i+1,
+      name: '北京京仪大酒店' + i,
+      address: '北京市海淀区大钟寺东路9号',
+      image: 'http://userimg.qunar.com/imgs/201410/25/GIGWX47MHTgwYKxg776.jpg',
+      star: '高档',
+      price: 100 * (i+1)
+    });
+  }
+
+  //res.setHeader('Content-Type', 'text/html');
+  res.end(JSON.stringify(data));
+}
